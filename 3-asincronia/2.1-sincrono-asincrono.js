3bb0f8facd11eac004aac772097ac115aba437d8
montoyaguzman <montoyaguzman7@gmail.com>
2022-08-17 21:34:28 -0500

    feat: solucion 1 callbacks

--- 3-asincronia/2.1-sincrono-asincrono.js
+++ 3-asincronia/2.1-sincrono-asincrono.js
@@ -3,10 +3,10 @@
  * Una linea se ejecuta inmediatamente despues de la otra
  * no hay codigo dependiente o bloqueante.
  */
-// console.log('============== BLOQUE 1 ==============');
-// console.log('Primero'); 
-// console.log('Segundo');
-// console.log('Tercero');
+console.log('============== BLOQUE 1 ==============');
+console.log('Primero'); 
+console.log('Segundo');
+console.log('Tercero');
 
 /**
  * BLOQUE 2 - Problema tipico de asincronia
@@ -15,7 +15,7 @@
 const validatingPayment = () => {
     setTimeout(() => {
         console.log('2 Validar la tarjeta de credito/debito y mostrar un loader');
-    }, 0)
+    }, 0);
 }
 
 console.log('1 Presionar el boton comprar');
---
+++ 3-asincronia/2.2-callbacks.js
@@ -0,0 +1,19 @@
+/**
+ * Bloque 3 - Solucion 1 Uso de callbacks
+ * Un callback es una funcion que se pasa como parametro a otra, con
+ * el fin de ejecutar en el momento que la necesitemos.
+ * NOTA: No es una buena practica por el anidamiento que produce (callback hell)
+ */
+const showSuccessMessage = () => {
+    console.log('3 Ver el mensaje de gracias por su compra joven');
+};
+
+ const validatingPayment = (showSuccessMessage) => {
+    setTimeout(() => {
+        console.log('2 Validar la tarjeta de credito/debito y mostrar un loader');
+        showSuccessMessage();
+    }, 0);
+};
+
+console.log('1 Presionar el boton comprar');
+validatingPayment(showSuccessMessage);
